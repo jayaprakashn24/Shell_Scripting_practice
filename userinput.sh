56216d33bc57694dc3e4f9786df193b9c43a41cf
jayaprakashn24 <jayaprakashn900@gmail.com>
2026-04-20 12:45:45 +0530

    userinput

---
+++ userinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Simple Shell Script to greet the user
+
+# Ask for the user's name
+echo "Enter your name:"
+read name
+
+# Print a greeting message
+echo "Hello, $name! Welcome to shell scripting."
